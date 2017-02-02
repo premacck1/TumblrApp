b210e69f31b6cc5a80c8215d8a77c17777be78ce
Mohit Singh <GrayShadow@Mohits-MacBook-Air.local>
2017-02-01 22:15:21 -0500

    Tumblr App

---
+++ PhotosViewController.swift
@@ -0,0 +1,112 @@
+//
+//  PhotosViewController.swift
+//  
+//
+//  Created by Mohit Singh on 2/1/17.
+//
+//
+
+import UIKit
+import AFNetworking
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var posts: [NSDictionary] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var photoCell: PhotoCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 240;
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        print(self.posts)
+                        self.tableView.reloadData()
+                    
+                    }
+                }
+        });
+        task.resume()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count;
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let post = posts[indexPath.row]
+        
+       // let photos = post.value(forKeyPath: "photos") as? [NSDictionary]
+        
+        if let photos = post.value(forKeyPath: "photos") as? [NSDictionary] {
+            // photos is NOT nil, go ahead and access element 0 and run the code in the curly braces
+            let imageUrlString = photos[0].value(forKeyPath: "original_size.url") as? String
+            
+//            let imageUrl = URL(string: imageUrlString!)!
+            
+            if let imageUrl = URL(string: imageUrlString!) {
+                // URL(string: imageUrlString!) is NOT nil, go ahead and unwrap it and assign it to imageUrl and run the code in the curly braces
+                cell.photoView.setImageWith(imageUrl)
+            } else {
+                // URL(string: imageUrlString!) is nil. Good thing we didn't try to unwrap it!
+            }
+            
+        } else {
+            // photos is nil. Good thing we didn't try to unwrap it!
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
